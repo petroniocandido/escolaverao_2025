081a8f3c58985942926145d40f7eb631b956a167
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2025-02-25 11:19:52 -0300

    Update distributed_mpi.py

--- distributed_mpi.py
+++ distributed_mpi.py
@@ -6,6 +6,9 @@ import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
 
+#################################################
+# OPERAÇÃO LINEAR EM LOTE VETORIZADA COM VMAP
+#################################################
 
 lote = 50000
 entradas = 100
@@ -22,64 +25,106 @@ def neuronio_linear(x,w,b):
 vmap_neuronio = torch.func.vmap(neuronio_linear, in_dims=0)
 
 
-def run(rank, size):
 
-    particao = int(lote // size)
+#################################################
+# ESSA É A FUNÇÃO QUE SERÁ EXECUTADA EM 
+# TODOS OS NÓS DO CLUSTER (EM CADA CORE/GPU)
+# rank: Identificador do processo. O rank 0 é 
+#       o processo-coordenador
+# size: Número de processos em execução
+#################################################
+
+def run(rank, size):  
+
+    particao = int(lote // size)      # Calcula o tamanho da partição de X para cada processo executor
 
     print(f"Partição: {particao}")
 
     print(f"Rank {rank} - World Size: {size}")
 
+    # Inicializa tensores vazios para receber os dados do processo coordenador
+    
     X = torch.empty(particao, entradas)
     W = torch.empty(neuronios, entradas)
     B = torch.empty(neuronios)
     y = torch.empty(particao, neuronios)
 
+    # Sincroniza todos os processos
+    
     dist.barrier(async_op=False)
 
+    ############################################################
+    # Bloco executado apenas pelo processo coordenador
+    ############################################################
     if rank == 0:
 
+        # Carrega os dados origianais a serem distribuídos entre os processos executores
         Xo = torch.rand(lote, entradas)
         Wo = torch.rand(neuronios, entradas)
         Bo = torch.zeros(neuronios)
 
+        # Divide o tensor X em partições a serem enviados aos processos executores
         lista_X = [Xo[i*particao:(i+1)*particao] for i in range(size)]
 
+        # Distribui as partições de X entre processos executores
+        # Deve haver uma função scatter para receber esses dados no bloco dos processos executores
         dist.scatter(tensor=X, scatter_list=lista_X, src=0, async_op=False)
 
         print(f"Rank {rank} - Scatter SEND X")
 
+        # Espalha W entre processos executores
+        # Deve haver uma função broadcast para receber esses dados no bloco dos processos executores
         dist.broadcast(Wo, src=0, async_op=False)
 
         print(f"Rank {rank} - Broadcast SEND W")
 
+        # Espalha B entre processos executores
+        # Deve haver uma função broadcast para receber esses dados no bloco dos processos executores
         dist.broadcast(Bo, src=0, async_op=False)
 
         print(f"Rank {rank} - Broadcast SEND B")
 
+        # Prepara uma lista de tensores vazios para receber os dados dos processos executores
         lista_y = [torch.empty(particao, neuronios) for k in range(size)]
 
+        
+        # Recolhe y dos processos executores
+        # Deve haver uma função gather para enviar esses dados no bloco dos processos executores
         dist.gather(y, lista_y, async_op=False)
 
         print(f"Rank {rank} - Gather RECV y")
 
+    ############################################################
+    # Bloco dos processos executores
+    ############################################################
     else:
+
+        # Coleta uma partição de X vinda do processo coordenador
+        # Deve haver uma função scatter para enviar esses dados no bloco do processo coordenador
         dist.scatter(tensor=X, src=0, async_op=False)
 
         print(f"Rank {rank} - Scatter RECV X")
 
+        # Coleta W vindo do processo coordenador
+        # Deve haver uma função broadcast para enviar esses dados no bloco do processo coordenador
         dist.broadcast(W, src=0, async_op=False)
 
         print(f"Rank {rank} - broadcast RECV W")
 
+        # Coleta B vindo do processo coordenador
+        # Deve haver uma função broadcast para enviar esses dados no bloco do processo coordenador
         dist.broadcast(B, src=0, async_op=False)
 
         print(f"Rank {rank} - broadcast RECV B")
 
+        # Executa a função linear y = W*X + B em lotes
+        
         linear_vmap = lambda X,W,B: vmap_neuronio(X, W.repeat(particao,1,1), B.repeat(particao,1))
 
         y = linear_vmap(X, W, B)
 
+        # Envia y vindo do processo coordenador
+        # Deve haver uma função gather para receber esses dados no bloco do processo coordenador
         dist.gather(y, dst=0, async_op=False)
 
         print(f"Rank {rank} - Gather y")
